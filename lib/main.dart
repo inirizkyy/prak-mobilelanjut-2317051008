b8c5fe16713b5a2f7fe2618efbf53512f28bccea
mrizkyprtmaaa <mrizkyprtmaaa@gmail.com>
2025-10-01 07:51:14 +0000

    Membuat tampilan dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
+import 'screens/dashboard_screen.dart';
 
-void main() => runApp(const JustduitApp());
+void main() {
+  runApp(const JustduitApp());
+}
 
 class JustduitApp extends StatelessWidget {
   const JustduitApp({super.key});
+
+  // sementara: misal user sudah login
+  final bool isLoggedIn = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +19,14 @@ class JustduitApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Justduit',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFF3F6F8), // abu-abu muda
+        scaffoldBackgroundColor: const Color(0xFFF3F6F8),
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1E88FF),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88FF)),
         inputDecorationTheme: InputDecorationTheme(
           isDense: true,
           filled: true,
           fillColor: Colors.white,
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 14,
-          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
             borderSide: const BorderSide(color: Color(0xFFE0E0E0)),
@@ -40,12 +41,14 @@ class JustduitApp extends StatelessWidget {
           ),
         ),
       ),
-      initialRoute: LoginScreen.route,
+      // cek status login → tentukan halaman pertama
+      initialRoute: isLoggedIn ? DashboardScreen.route : LoginScreen.route,
       routes: {
-        '/': (context) => const LoginScreen(), // default -> Sign In
-        LoginScreen.route: (context) => const LoginScreen(),
-        SignupScreen.route: (context) => const SignupScreen(),
+        LoginScreen.route: (_) => const LoginScreen(),
+        SignupScreen.route: (_) => const SignupScreen(),
+        DashboardScreen.route: (_) => const DashboardScreen(),
       },
     );
   }
 }
+
